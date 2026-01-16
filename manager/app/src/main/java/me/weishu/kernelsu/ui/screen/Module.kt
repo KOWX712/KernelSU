e8d68c84fdf129ed22120f5e035ebcba35d0b110
KOWX712 <leecc0503@gmail.com>
2026-01-16 21:17:21 +0800

    manager: ui/ux improvement
    this is a squash of:
    
    * manager: improve superuser screen ux (KOWX712/KernelSU@c6137d3e3dbe7d2fc5bf4925add669af05563bc6)
    
    - Refresh immediately after changing app profile.
    - Do not scroll to top automatically.
    - Scroll to top on manual refresh.
    
    * manager: improve overall ui (KOWX712/KernelSU@b4fb40ea6d8ea464d1f8bf384280e381691b2b64)
    
    Adjust content text color.
    
    * manager: allow module update check when update flag already exist (KOWX712/KernelSU@d23d9b25da2086919e603b14b9cbefb96cac1139)
    
    * manager: proper refresh update status after updating module (KOWX712/KernelSU@fec7eb023d9b02abd2a932390baf5888963ea4fb)
    
    * manager: fix abnormal width between module card button on large device (KOWX712/KernelSU@4f652df7ffd7020855fc69fafcd03db6b923837d)
    
    * manager: remove loading indicator from module operation (KOWX712/KernelSU@f61041634f2f0927522006cee4a2a1647bed2260)
    
    since we're not using overlayfs, module operation is fast now, let's remove the annoying loading indicator from module on/off/uninstall operation
    
    Signed-off-by: KOWX712 <leecc0503@gmail.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Module.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Module.kt
@@ -29,6 +29,7 @@ import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.toggleable
@@ -81,6 +82,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
@@ -169,7 +171,7 @@ fun ModuleScreen(navigator: DestinationsNavigator) {
                 title = { Text(stringResource(R.string.module)) },
                 searchText = viewModel.search,
                 onSearchTextChange = { viewModel.search = it },
-                onClearClick = { viewModel.search = "" },
+                onClearClick = { viewModel.search = TextFieldValue("") },
                 actionsContent = {
                     IconButton(
                         onClick = { navigator.navigate(ModuleRepoScreenDestination) }
@@ -415,13 +417,11 @@ private fun ModuleList(
             }
         }
 
-        val success = loadingDialog.withLoading {
-            withContext(Dispatchers.IO) {
-                if (isUninstall) {
-                    uninstallModule(module.id)
-                } else {
-                    undoUninstallModule(module.id)
-                }
+        val success = withContext(Dispatchers.IO) {
+            if (isUninstall) {
+                uninstallModule(module.id)
+            } else {
+                undoUninstallModule(module.id)
             }
         }
 
@@ -497,10 +497,8 @@ private fun ModuleList(
                             },
                             onCheckChanged = {
                                 scope.launch {
-                                    val success = loadingDialog.withLoading {
-                                        withContext(Dispatchers.IO) {
-                                            toggleModule(module.id, !module.enabled)
-                                        }
+                                    val success = withContext(Dispatchers.IO) {
+                                        toggleModule(module.id, !module.enabled)
                                     }
                                     if (success) {
                                         viewModel.fetchModuleList()
@@ -600,6 +598,7 @@ fun ModuleItem(
 
                     Text(
                         text = "$moduleVersion: ${module.version}",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
                         fontSize = MaterialTheme.typography.bodySmall.fontSize,
                         lineHeight = MaterialTheme.typography.bodySmall.lineHeight,
                         fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
@@ -608,6 +607,7 @@ fun ModuleItem(
 
                     Text(
                         text = "$moduleAuthor: ${module.author}",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
                         fontSize = MaterialTheme.typography.bodySmall.fontSize,
                         lineHeight = MaterialTheme.typography.bodySmall.lineHeight,
                         fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
@@ -634,6 +634,7 @@ fun ModuleItem(
 
             Text(
                 text = module.description,
+                color = MaterialTheme.colorScheme.outline,
                 fontSize = MaterialTheme.typography.bodySmall.fontSize,
                 fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
                 lineHeight = MaterialTheme.typography.bodySmall.lineHeight,
@@ -750,7 +751,7 @@ fun ModuleItem(
                             }
                         }
 
-                        Spacer(modifier = Modifier.weight(0.1f, true))
+                        Spacer(Modifier.width(12.dp))
                     }
                 }
 
